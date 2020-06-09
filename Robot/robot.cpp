2fa684eaf901586100eff6ca02053130d3ae7163
Harrison-BC <65011243+Harrison-BC@users.noreply.github.com>
2020-06-09 15:13:37 +1200

    Robot stopping
    I've implemented code for the robot stopping when it reaches the finish line.

--- Robot/robot.cpp
+++ Robot/robot.cpp
@@ -1,4 +1,8 @@
 #include "robot.hpp"
+  
+// count of black pixels (not good idea to put here)
+int blackIndexCount = 0;
+
 // structure so that processImage can return two values
 struct Result
 {
@@ -15,15 +19,18 @@ void moveRobot(Result result)
   // difference based on error
   int dv = 0;
   // if the robot isn't lost, dv is error/2
-  if (result.lost == 0)
-  {
+  if (result.lost == 0) {
     dv = result.error / 2;
     // if the robot is lost, dv is large so that it turns around
   }
-  else if (result.lost == 1)
-  {
+  else if (result.lost == 1) {
     dv = 50;
-  }
+  } 
+	// sets speed of motors to 0
+  else if (result.lost == 2){
+	v = 0;
+	dv = 0;
+}
   // set left motor to default + dv
   double vLeft = v + dv;
   // set right motor to default - dv
@@ -32,8 +39,7 @@ void moveRobot(Result result)
   setMotors(vLeft, vRight);
 }
 // process the image from the camera
-Result processImage()
-{
+Result processImage(){
   // create a result structure to hold values
   Result result;
   takePicture();
@@ -41,9 +47,9 @@ Result processImage()
   int height = cameraView.height;
   // get the middle row
   int row = width / 2;
+  
   // loop forever (probably not ideal)
-  while (1)
-  {
+  while (1) {
     // by default robot is not lost
     int lost = 0;
     // index of white pixels
@@ -52,22 +58,32 @@ Result processImage()
     int whiteIndexTotal = 0;
     // count of white pixels
     int whiteIndexCount = 0;
+
     usleep(10000);
     takePicture();
     // loop across the middle row
-    for (int column = 0; column < width; column++)
+	for (int column = 0; column < width; column++)
     {
-      // get the whiteness of each pixel
-      int white = (int)get_pixel(cameraView, row, column, 3);
-      // if whiteness is greater than 200, it's white (bad way of doing it)
-      if (white > 200)
-      {
-        whiteIndexTotal += column;
-        whiteIndexCount += 1;
-      }
+      // get the luminosity of each pixel
+		int luminosity = (int)get_pixel(cameraView, row, column, 3);
+      // if luminosity is greater than 200, it's white (bad way of doing it)
+		if (luminosity > 200) {
+			whiteIndexTotal += column;
+			whiteIndexCount += 1;
+		} 
+	  // if luminosity is less than 20, it's black
+		else if (luminosity < 20){
+			blackIndexCount = blackIndexCount + 1;
+			//std::cout << "blackIndexCount: "<<blackIndexCount<<std::endl;
+		}
+
     }
+	// If the image has black pixels, then lost = 2.
+	if (blackIndexCount > 20) {
+		lost = 2;
+	}
     // If there are any white pixels, average their indexes to find out where in the row they are
-    if (whiteIndexCount != 0)
+    else if (whiteIndexCount != 0)
     {
       whiteIndex = whiteIndexTotal / whiteIndexCount;
       // if there aren't any white pixels, the robot is lost
@@ -76,6 +92,7 @@ Result processImage()
     {
       lost = 1;
     }
+    
     // set the error to the index minus the width over 2
     // this is so that if the white line is on the left, error is negative
     // and if it's on the right, error is positive
@@ -86,11 +103,9 @@ Result processImage()
   }
 }
 
-int main()
-{
-  if (initClientRobot() != 0)
-  {
-    std::cout << " Error initializing robot" << std::endl;
-  }
-  processImage();
+int main() {
+	if (initClientRobot() != 0) {
+		std::cout << " Error initializing robot" << std::endl;
+	}
+	processImage();
 }
